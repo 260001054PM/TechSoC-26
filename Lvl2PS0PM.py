a091f94c48c7b20e530ba85d079c74b4ff4699a5
Pranshul Mankad <cse260001054@iiti.ac.in>
2026-08-16 12:30:57 +0530

    Sorting Weights

--- Lvl2PS0PM.py
+++ Lvl2PS0PM.py
@@ -1,25 +1,39 @@
 Morship = "Y"
+def sorting(list):
+  print("Sorted Way Of Weights: ")
+  max = 0 #Since weight entered is positive
+  sortedlist = []
+  while list:
+    for i in list:
+      if i > max:
+        max = i
+    list.remove(max)
+    sortedlist.append(max)
+    max = 0
+  for j in range(len(sortedlist)-1, -1, -1):
+    print(sortedlist[j], end = " ")
 while Morship != "N":
-  C = float(input())
-  N = int(input())
+  C = float(input("Enter Maximum Storage Capacity: "))
+  N = int(input("Enter the Number of Containers: "))
   weights = []
   for i in range(N):
-    wi = int(input())
-    weights.append(wi)
+    wi = int(input("Enter Weight: "))
+    weights.append(wi)
   print("Total shipment weight: ", sum(weights))
-  print("Average container weight: ", sum(weights)/len(weights))
+  print("Average container weight: ", round(sum(weights)/len(weights), 2))
   print("Heaviest container: ", max(weights))
   print("Lightest container: ", min(weights))
   print("Classification: ", end = "")
   if sum(weights) >= 200:
-    print("Heavy")
+    print("Heavy")
   else:
-    print("Light")
+    print("Light")
   print("Port Capacity: ", C)
   print("Status: ", end = "")
   if sum(weights) <= C:
-    print("Shipment can be unloaded")
+    print("Shipment can be unloaded")
   else:
-    print("Shipment exceeds port capacity")
+    print("Shipment exceeds port capacity")
+  sorting(weights)
   Morship = input("Do you want to process more ships? (Y/N): ")
 print("Thank You")
